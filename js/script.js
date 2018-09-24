549d948d143c18bbf0cd6cb96e15504844f3c2eb
Luis Waderdorff <walderdorff@live.de>
2018-09-24 16:35:33 +0200

    Fertig (for feedback)

--- js/script.js
+++ js/script.js
@@ -20,9 +20,9 @@ $( ".remove").click(function() {
 })
 
 
-// Chart Traffic
+// Chart Traffic Weekly
 
-var traffic = $("#chartTraffic");
+var traffic = $("#chartTraffic-weekly");
 
 
 
@@ -31,8 +31,125 @@ var chartTraffic = new Chart(traffic, {
     data: {
         labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
         datasets: [{
-            label: "number of visitors",
+            label: "number of visitors weekly",
             data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 1250],
+            borderColor: "#361f58",
+            backgroundColor: "rgba(101, 57, 164, 0.4)",
+            borderWidth: 1
+        }]
+    },
+    options: {
+      layout: {
+
+      },
+      responsive:true,
+      maintainAspectRatio: false,
+      elements: {
+        line: {
+            tension: 0
+        }
+    },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+// Chart Traffic Daily
+
+var trafficDaily = $("#chartTraffic-daily");
+
+
+
+var chartTrafficDaily = new Chart(trafficDaily, {
+    type: 'line',
+    data: {
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
+        datasets: [{
+            label: "number of visitors daily",
+            data: [75, 125, 100, 150, 200, 150, 175],
+            borderColor: "#361f58",
+            backgroundColor: "rgba(101, 57, 164, 0.4)",
+            borderWidth: 1
+        }]
+    },
+    options: {
+      layout: {
+
+      },
+      responsive:true,
+      maintainAspectRatio: false,
+      elements: {
+        line: {
+            tension: 0
+        }
+    },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+// Chart Traffic Hourly
+
+var trafficHourly = $("#chartTraffic-hourly");
+
+
+
+var chartTrafficHourly = new Chart(trafficHourly, {
+    type: 'line',
+    data: {
+        labels: ["1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm", "12pm"],
+        datasets: [{
+            label: "number of visitors hourly",
+            data: [75, 125, 100, 150, 200, 150, 175, 75, 125, 100, 150, 200, 150, 175, 75, 125, 100, 150, 200, 150, 175, 75, 125, 100],
+            borderColor: "#361f58",
+            backgroundColor: "rgba(101, 57, 164, 0.4)",
+            borderWidth: 1
+        }]
+    },
+    options: {
+      layout: {
+
+      },
+      responsive:true,
+      maintainAspectRatio: false,
+      elements: {
+        line: {
+            tension: 0
+        }
+    },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+// Chart Traffic Monthly
+
+var trafficMonthly = $("#chartTraffic-monthly");
+
+
+
+var chartTrafficMonthly = new Chart(trafficMonthly, {
+    type: 'line',
+    data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [{
+            label: "number of visitors monthly",
+            data: [2000, 3500, 4000, 3750, 3500, 4500, 2250, 3000, 3150, 4250, 4750, 5000],
             borderColor: "#361f58",
             backgroundColor: "rgba(101, 57, 164, 0.4)",
             borderWidth: 1
@@ -123,5 +240,113 @@ var chartTraffic = new Chart(mobile, {
         position: "bottom",
       }
         }
-    })
-;
+    });
+
+// Send buttons
+
+$( "#user-send").click(function() {
+  if ($("#user-search").val() && $("#user-message").val()) {
+    alert("Your message has been sent.");
+  } else {
+    alert("You havent filled out all of the required fields.");
+  }
+})
+
+// Traffic buttons
+
+$("#time-hourly").click(function() {
+  $(".wrapper-1").hide();
+  $(".wrapper-3").hide();
+  $(".wrapper-4").hide();
+  $(".wrapper-2").show();
+  $("#time-hourly").addClass("green-background");
+  $("#time-daily").removeClass("green-background");
+  $("#time-weekly").removeClass("green-background");
+  $("#time-monthly").removeClass("green-background");
+})
+
+$("#time-daily").click(function() {
+  $(".wrapper-1").hide();
+  $(".wrapper-2").hide();
+  $(".wrapper-4").hide();
+  $(".wrapper-3").show();
+  $("#time-hourly").removeClass("green-background");
+  $("#time-daily").addClass("green-background");
+  $("#time-weekly").removeClass("green-background");
+  $("#time-monthly").removeClass("green-background");
+})
+
+$("#time-weekly").click(function() {
+  $(".wrapper-2").hide();
+  $(".wrapper-3").hide();
+  $(".wrapper-4").hide();
+  $(".wrapper-1").show();
+  $("#time-hourly").removeClass("green-background");
+  $("#time-daily").removeClass("green-background");
+  $("#time-weekly").addClass("green-background");
+  $("#time-monthly").removeClass("green-background");
+})
+
+$("#time-monthly").click(function() {
+  $(".wrapper-1").hide();
+  $(".wrapper-3").hide();
+  $(".wrapper-2").hide();
+  $(".wrapper-4").show();
+  $("#time-hourly").removeClass("green-background");
+  $("#time-daily").removeClass("green-background");
+  $("#time-weekly").removeClass("green-background");
+  $("#time-monthly").addClass("green-background");
+})
+
+// Autocomplete
+
+const names = {
+  data: ["Victoria Chambers", "Dale Byrd", "Dawn Wood", "Dan Oliver"],
+  list: {
+		match: {
+			enabled: true
+		}
+	}
+};
+
+$("#user-search").easyAutocomplete(names);
+
+// Save Settings to local Storage
+
+$("#save").click( function () {
+  localStorage.setItem("timezone", $("#timezone").val());
+  localStorage.setItem("check1", document.getElementById("myonoffswitch-1").checked);
+  localStorage.setItem("check2", document.getElementById("myonoffswitch-2").checked);
+});
+
+//First Checkbox
+
+let checkBoolean1;
+
+// Transforming localStorage string to boolean
+
+if ( localStorage.getItem("check1") === "true") {
+  checkBoolean1 = true;
+} else {
+  checkBoolean1 = false;
+}
+
+// On page load set the checkValue to the transformed getItem
+
+document.getElementById("myonoffswitch-1").checked = checkBoolean1;
+
+let checkBoolean2;
+
+if ( localStorage.getItem("check2") === "true") {
+  checkBoolean1 = true;
+} else {
+  checkBoolean1 = false;
+}
+
+document.getElementById("myonoffswitch-2").checked = checkBoolean1;
+
+// Set the value of the dropdownmenue to the one saved during the click to the local Storage
+
+$('#timezone').val(localStorage.getItem('timezone'));
+
+// Why does changing the value make it select that specific option from the select input?
